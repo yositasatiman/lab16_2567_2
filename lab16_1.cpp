4e5ea5a0cc8614a2eb0df122d63d70b2d622811d
Yosita satiman <yosita_satiman@cmu.ac.th>
2025-01-31 15:22:27 +0700

    lab16

--- lab16_1.cpp
+++ lab16_1.cpp
@@ -32,3 +32,38 @@ int main(){
 	findColSum(dPtr,sum2,N,M); 
 	showData(sum2,1,M);
 }
+
+void showData(double a[], int N, int M){
+    cout << fixed << setprecision(2);
+    for(int i = 0; i < N*M; i++){
+        cout << a[i];
+        if((i+1)%M == 0) cout << endl;
+        else cout << " ";
+    }
+}
+
+void randData(double a[], int N, int M){
+    for(int i = 0; i < N*M; i++){
+        a[i] = rand()%101/100.00;
+    }
+}
+
+void findRowSum(const double a[], double b[], int M, int N){
+    for(int i = 0; i < M; i++){
+        double sum = 0;
+        for(int j = 0; j < N; j++){
+            sum += a[(i * N) + j];
+        }
+        b[i] = sum;
+    }
+}
+
+void findColSum(const double a[], double b[], int N, int M){
+    for(int i = 0; i < M; i++){
+        double sum = 0;
+        for(int j = 0; j < N; j++){
+            sum += a[(j * M) + i];
+        }
+        b[i] = sum;
+    }
+}
